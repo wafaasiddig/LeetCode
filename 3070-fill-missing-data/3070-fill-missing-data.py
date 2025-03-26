75a001b2aed0fbf1918f4fbb23a2c9411ccccf6f
Wafaa_Siddig <116094810+wafaasiddig@users.noreply.github.com>
2025-03-26 05:25:24 +0300

    Time: 276 ms (95.03%), Space: 66 MB (12.74%) - LeetHub

--- 3070-fill-missing-data/3070-fill-missing-data.py
+++ 3070-fill-missing-data/3070-fill-missing-data.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
-    products['quantity']=products['quantity'].fillna(0)
-    return products
+    return products.assign(quantity= products['quantity'].fillna(0))
